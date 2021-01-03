0b35e18b32cc8b5217cc1ada9546b080fd88b621
Hugo Curado <hfsvbc@hugocurado.info>
2021-01-03 17:37:39 +0000

    add transcode_media service

--- app/services/transcode_media.rb
+++ app/services/transcode_media.rb
@@ -1,9 +1,13 @@
+require "streamio-ffmpeg"
+
 class TranscodeMedia
   def initialize(params)
-    @media = params(:media)
+    @media = params.fetch(:media)
+    @profile = params.fetch(:profile)
   end
-  
+
   def call
-    
+    movie = FFMPEG::Movie.new(@media)
+    movie.transcode("/media/test.mp4", @profile.config) { |progress| puts progress }
   end
 end
