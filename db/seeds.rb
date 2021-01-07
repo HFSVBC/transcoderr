a7dfc43598bb270da17f85d92ff3a152ee8d3691
Hugo Curado <hfsvbc@hugocurado.info>
2021-01-07 23:30:29 +0000

    refactor entrypoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Profile.create(
-  name: "h265",
-  config: { video_codec: "libx265" },
-)
+Profile.find_or_create_by(name: "h265") do |profile|
+  profile.config = { video_codec: "libx265" }
+end
