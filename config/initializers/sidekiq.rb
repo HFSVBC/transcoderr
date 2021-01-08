041f9c3f9511d00e77b7b1b3c05ddb1c080daf22
Hugo Curado <hfsvbc@hugocurado.info>
2021-01-08 18:05:28 +0000

    change sidekiq statuses expiration to 1 week

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ require "sidekiq-scheduler"
 require "sidekiq-status"
 
 Sidekiq.configure_client do |config|
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 1.week
 end
 
 Sidekiq.configure_server do |config|
@@ -11,7 +11,7 @@ Sidekiq.configure_server do |config|
     Sidekiq.schedule = YAML.load_file(File.expand_path("../sidekiq_scheduler.yml", __dir__))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
 
-    Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
-    Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+    Sidekiq::Status.configure_server_middleware config, expiration: 1.week
+    Sidekiq::Status.configure_client_middleware config, expiration: 1.week
   end
 end
