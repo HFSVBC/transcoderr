e7a405d91323a8cf28f6be85757b415ee7c8d77e
Hugo Curado <hfsvbc@hugocurado.info>
2021-01-10 22:55:23 +0000

    add subtitle encoder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 profile = Profile.find_or_initialize_by(name: "h265")
-profile.config = { video_codec: "libx265", custom: %w(-map 0 -crf 20) }
+profile.config = { video_codec: "libx265", custom: %w(-map 0 -c:s mov_text -crf 20) }
 profile.save!
